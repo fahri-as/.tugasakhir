156cbc315cb369f5d8ba03f1b384d339a69ada82
Fahri Andika Sanjaya <2111523006_fahri@student.unand.ac.id>
2025-05-23 18:57:40 +0700

    Update navigation.blade.php

--- TA/resources/views/layouts/navigation.blade.php
+++ TA/resources/views/layouts/navigation.blade.php
@@ -59,10 +59,7 @@
                                 <i class="fas fa-chart-line mr-2 text-gray-400"></i>
                                 Evaluations
                             </x-dropdown-link>
-                            <x-dropdown-link :href="route('criteria.index')" class="flex items-center px-4 py-2 text-sm transition-all duration-300 hover:bg-indigo-50">
-                                <i class="fas fa-clipboard-list mr-2 text-gray-400"></i>
-                                Criteria
-                            </x-dropdown-link>
+                        
                         </x-slot>
                     </x-dropdown>
                 </div>
