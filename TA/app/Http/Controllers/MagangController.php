69bf564f4cc715555ef1d768602aa968aadfa7ff
Fahri Andika Sanjaya <2111523006_fahri@student.unand.ac.id>
2025-05-30 18:18:16 +0700

    search intern

--- TA/app/Http/Controllers/MagangController.php
+++ TA/app/Http/Controllers/MagangController.php
@@ -100,6 +100,14 @@ class MagangController extends Controller
             $query->whereIn('status_seleksi', $request->statuses);
         }
 
+        // Search by applicant name if search parameter is provided
+        if ($request->filled('search')) {
+            $searchTerm = $request->search;
+            $query->whereHas('pelamar', function($q) use ($searchTerm) {
+                $q->where('nama', 'LIKE', "%{$searchTerm}%");
+            });
+        }
+
         // Handle sorting
         $sortBy = $request->input('sort_by', 'total_skor');
         $sortDir = $request->input('sort_dir', 'desc');
